75b762c984516aa631345f0caaafca450e237654
JihoYeom <jiho6493@gmail.com>
2018-06-19 14:50:48 +0900

    inline-style적용. 주석도 달았어용

--- first-react-app/src/components/SimpleComponent.js
+++ first-react-app/src/components/SimpleComponent.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 
+//css보다는 inline-style을 권장.
+const style = {
+    fontSize:24,
+    fontWeight:600
+}
+
 export default class SimpleComponent extends Component {
-    render = () => <div>it's simple!</div>;
+    //arrow function으로도 가능하다.
+    render = () => <div style={style}>it's simple!</div>;
 }
