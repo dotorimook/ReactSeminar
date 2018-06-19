3ca77e3ceecaa482203fcf798d27cd5d3e98f6a4
JihoYeom <jiho6493@gmail.com>
2018-06-19 16:37:20 +0900

    react-router 적용

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SimpleComponent from './components/SimpleComponent';
+import {BrowserRouter, NavLink, Route} from 'react-router-dom';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
+import Component3 from './components/Component3';
 
 class App extends Component {
   constructor (props) {
@@ -23,15 +27,25 @@ class App extends Component {
     };
     const {msg} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {msg}
-        </p>
-        <SimpleComponent name='you' data={foo} onUpdate={this.updateMsg} />
+      <div>
+        <BrowserRouter>
+          <div>
+            <ul id='menu'>
+              <li>
+                <NavLink to="/1">component 1</NavLink>
+              </li>
+              <li>
+                <NavLink to="/2">component 2</NavLink>
+              </li>
+              <li>
+                <NavLink to="/3">component 3</NavLink>
+              </li>
+            </ul>
+            <Route path="/1" component={Component1}/>
+            <Route path="/2" component={Component2}/>
+            <Route path="/3" component={Component3}/>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ first-react-app/src/components/Component1.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+export default class Component1 extends Component {
+    render = () => <div>Component1</div>
+}
